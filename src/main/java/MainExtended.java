2f14e43a56a030ad1bf5853f7e43cea2f4e005bf
Beata Cygan <beata.cygan93@gmail.com>
2021-11-11 12:48:51 +0100

    Zmienne

---
+++ src/main/java/MainExtended.java
@@ -0,0 +1,18 @@
+public class MainExtended {
+    public static void main(String[] args) {
+    String myDogName = "Barry";
+    int myDogAge = 10;
+    double myDogHeight = 0.50;
+    char fridgeClass = 'A';
+    boolean isGoodDog = true;
+    int myLuckyNumber = 17;
+    System.out.println(myDogName);
+            System.out.println(myDogAge);
+            System.out.println(myDogHeight);
+            System.out.println(fridgeClass);
+            System.out.println(isGoodDog);
+            System.out.println(myLuckyNumber);
+            }
+}
+
+
