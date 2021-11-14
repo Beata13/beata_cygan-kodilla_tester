d7f564467180f99ed5e4b093b8e8c8fdfcac4e00
Beata Cygan <beata.cygan93@gmail.com>
2021-11-14 12:19:40 +0100

    Zadania

---
+++ src/main/java/SimpleArray.java
@@ -0,0 +1,13 @@
+public class SimpleArray
+{  public static void main(String[] args) {
+    String[] colour = new String[5];
+colour[0] = "blue";
+colour[1] = "red";
+colour[2] = "orange";
+colour[3] = "green";
+colour [4] = "black";
+int numberOfElements = colour.length;
+
+    System.out.println("Moja tablia zawiera" + " " +numberOfElements + " " + "elementów");
+}
+}
