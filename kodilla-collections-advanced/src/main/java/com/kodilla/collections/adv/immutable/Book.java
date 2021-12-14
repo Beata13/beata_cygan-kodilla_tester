30077ababcf6768e64a06065151bc9f5b41c50e7
Beata Cygan <beata.cygan93@gmail.com>
2021-12-14 21:30:48 +0100

    Task

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/Book.java
@@ -0,0 +1,17 @@
+package com.kodilla.collections.adv.immutable;
+
+public class Book {
+    public class Book {
+        protected final String author;
+        protected final String title;
+
+        public Book(String author, String title) {
+            this.author = author;
+            this.title = title;
+        }
+
+        public final tring getAuthor() { return  author; }
+
+        public final String getTitle() { return title; }
+    }
+}
---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/Task.java
@@ -0,0 +1,12 @@
+package com.kodilla.collections.adv.immutable.homework;
+
+public final class  Task {
+     final   String tittle;
+    final   int duration;
+    public Task(String tittle, int duration) {
+        this.tittle = tittle;
+        this.duration = duration;
+    }
+
+
+}
