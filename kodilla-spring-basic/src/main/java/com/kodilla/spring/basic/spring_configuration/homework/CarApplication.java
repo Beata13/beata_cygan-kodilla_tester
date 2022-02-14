455c9d82657744883d284a3777c1e00880e8324a
Beata Cygan <beata.cygan93@gmail.com>
2022-02-14 19:22:05 +0100

    CarApplication

--- kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/CarApplication.java
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_configuration/homework/CarApplication.java
@@ -1,43 +1,40 @@
-//package com.kodilla.spring.basic.spring_configuration.homework;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import java.util.Random;
-//
-//@Configuration
-//public class CarApplication {
-//    @Bean
-//    public Car whichCarIsInUse() {
-//        Car car;
-//        Random generator = new Random();
-//        int season = generator.nextInt(5);
-//        if (season == 1) {
-//            car = new SUV();
-//        } else if (season == 2) {
-////            car = new Cabrio();
-//
-//        } else {
-//            car = new Sedan();
-//        }
-//        return car;
-//    }
-//
-//    @Bean
-//    public boolean LightsTurnOnAndOff() {
-//        Car car;
-//        Random generator = new Random();
-//        int hour = generator.nextInt(25);
-//        if (hour >= 0 && hour <= 6) {
-//            car.hasHeadlightsTurnedOn() == true;
-//
-//        }
-//        if (hour >= 20 && hour <= 24) {
-//            car.hasHeadlightsTurnedOn() == true;
-//        } else {
-//            return false;
-//        }
-//        return false;
-//    }
-//}
-//
+package com.kodilla.spring.basic.spring_configuration.homework;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalTime;
+import java.util.Random;
+
+@Configuration
+public class CarApplication {
+    @Bean
+    public Car whichCarIsInUse() {
+        Car car;
+        Random generator = new Random();
+        int season = generator.nextInt(5);
+        if (season == 1) {
+            car = new SUV();
+        } else if (season == 2) {
+            car = new Cabrio();
+
+        } else {
+            car = new Sedan();
+        }
+        return car;
+    }
+
+    @Bean
+    public void LightsTurnOnAndOff() {
+        Car car;
+        LocalTime time = LocalTime.now();
+        LocalTime startTime = LocalTime.parse("06:00:00");
+        LocalTime stopTime = LocalTime.parse("20:00:00");
+        if (time.isAfter(startTime) && time.isBefore(stopTime)) ;
+        {
+            System.out.println("Lights off");
+        }
+        if (time.isAfter(stopTime) && time.isBefore(startTime)) ;
+        System.out.println("Lights on");
+    }
+}
--- kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_configuration/homework/CarApplicationTestSuite.java
+++ kodilla-spring-basic/src/test/java/com/kodilla/spring/basic/spring_configuration/homework/CarApplicationTestSuite.java
@@ -6,17 +6,30 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootTest
 class CarApplicationTestSuite {
-//    @Test
-//    public void shouldChooseCorrectCar() {
-//        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic.spring_configuration.homework");
-//Car car = (Car) context.getBean("whichCarIsInUse");
-//        String carType = car.getCarType();
-//        System.out.println(carType);
-//        Assertions.assertEquals("SUV", carType);
-//
-//
-//    }
-}
+    @Test
+    public void shouldChooseCorrectCar() {
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic.spring_configuration.homework");
+Car car = (Car) context.getBean("whichCarIsInUse");
+        String carType = car.getCarType();
+        System.out.println(carType);
+List<String> possibleCarType = Arrays.asList("SUV", "Cabrio", "Sedan" );
+        Assertions.assertTrue(possibleCarType.contains(carType));
+
+
+   }
+    @Test
+    public void shouldTurnOffLights() {
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.basic.spring_configuration.homework");
+        Car car = (Car) context.getBean("LightsTurnOnAndOff");
+        boolean lightsStatus = car.hasHeadlightsTurnedOn();
+        System.out.println(lightsStatus);
+
+        Assertions.assertEquals("Lights off", lightsStatus);
+
+    }
+    }
