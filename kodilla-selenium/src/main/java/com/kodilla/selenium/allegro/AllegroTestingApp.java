a5baa09b0484a7f4f82e4182c5e044a3e037ce12
Beata Cygan <beata.cygan93@gmail.com>
2022-02-25 17:22:52 +0100

    Selenium

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -7,20 +7,25 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
+import java.util.List;
+
 public class AllegroTestingApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\Selenium-drivers\\Chrome\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.ebay.pl/");
 
-        WebElement inputField = driver.findElement(By.xpath("//*[@id=\"gh-ac-box2\"]/input"));
+        WebElement inputField = driver.findElement(By.cssSelector(".gh-td-s>div>div>input"));
         inputField.sendKeys("Mavic mini");
         inputField.submit();
+        inputField.wait(1000);
 
-        WebElement productCategory = driver.findElement(By.xpath("//*[id=\"gh-cat-box\"]/select"));
+        WebElement productCategory = driver.findElement(By.cssSelector("#gh-cat-td>div>select"));
         Select dronCategory = new Select(productCategory);
         dronCategory.selectByIndex(1);
 
+        List<WebElement> element = driver.findElements(By.cssSelector(".gh-td-s>div>div>input"));
+element.get(0).getText();
 
     }
 }
